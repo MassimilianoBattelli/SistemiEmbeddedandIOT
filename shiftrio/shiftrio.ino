fb0a9a4fc675d1368bdccf080aa354b5cb6ab107
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2023-02-09 19:43:28 +0100

    commit

---
+++ shiftrio/shiftrio.ino
@@ -0,0 +1,61 @@
+#include <ESP8266WiFi.h>
+#include <MQTT.h>
+
+const char ssid[] = "";
+const char pass[] = "";
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("ESP8266", "iotunibo", "Vq95BxxTQkHlkcJS")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("Topic");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println(topic + ": " + payload);
+}
+
+void setup() {
+  Serial.begin(115200);
+  // start wifi and mqtt
+  WiFi.begin(ssid, pass);
+  client.begin("iotunibo.cloud.shiftr.io", net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
+
+void loop() {
+  client.loop();
+  delay(10);
+
+  // check if connected
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    int temperature=random(-10,10);
+    char cstr[2];
+    itoa(temperature, cstr, 10);
+    lastMillis = millis();
+    client.publish("/Topic", cstr);
+  }
+}
